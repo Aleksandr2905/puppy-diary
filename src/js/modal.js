dbb6b894b2da1af1a3fdea03b09380e033bfbbb1
Aleksandr2905 <natalukha@ukr.net>
2024-08-03 14:52:48 +0300

    added header, footer & modal

---
+++ src/js/modal.js
@@ -0,0 +1,20 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    modalItems: document.querySelectorAll('.modal-item'),
+  };
+
+  refs.openModalBtn.forEach(btn => {
+    btn.addEventListener('click', toggleModal);
+  });
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.modalItems.forEach(item => {
+    item.addEventListener('click', toggleModal);
+  });
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
